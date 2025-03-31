c61bbe9d38e3d111cb46c596d9a00bcce9736ef8
Vitaliy <komissarov.v.2003@mail.ru>
2025-03-31 16:22:37 +0300

    feat: update tokenize and lemmatization

--- src/main/java/ru/kpfu/itis/task2/HtmlParser.java
+++ src/main/java/ru/kpfu/itis/task2/HtmlParser.java
@@ -9,33 +9,57 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import ru.kpfu.itis.task2.exception.ResourceException;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.*;
 import java.util.regex.Pattern;
 
 public class HtmlParser {
     private static final String PAGE_FILENAME_FORMAT = "./pages/page_%d.html";
     private static final String STOP_WORDS_FILENAME = "stopwords.txt";
-    private static final Set<String> STOP_WORDS = read();
+    private static final String ENGLISH_WORDS_FILENAME = "words_alpha.txt";
+    private static final Set<String> STOP_WORDS = read(STOP_WORDS_FILENAME);
+    private static final Set<String> ENGLISH_WORDS = read(ENGLISH_WORDS_FILENAME);
     private static final Pattern WORD_PATTERN = Pattern.compile("\\b[a-zA-Z]+\\b");
 
     public static void main(String[] args) throws IOException {
-        //Получения токенов
-        Set<String> tokens = tokenizeHtmlFiles();
-        //Сохранение токенов
-        saveTokens(tokens);
+        HtmlParser parser = new HtmlParser();
+        parser.parse();
+    }
+
+    public void parse() throws IOException {
+        //Получения текстов со страниц
+        Set<String> texts = parseText();
 
         Properties props = new Properties();
         props.setProperty("annotators", "tokenize, ssplit, pos, lemma");
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
         //Распределение токенов по леммам
-        Map<String, Set<String>> lemmatizedTokens = lemmatizeTokens(tokens, pipeline);
+        Map<String, Set<String>> lemmatizedTokens = lemmatizeText(texts, pipeline);
+
         //Сохранение лемм
         saveLemmas(lemmatizedTokens);
+
+        Set<String> tokens = new TreeSet<>();
+
+        //Сортирвока токенов
+        for (Map.Entry<String,Set<String>> entry : lemmatizedTokens.entrySet()) {
+            tokens.addAll(entry.getValue());
+        }
+
+        //Сохранение токенов
+        saveTokens(tokens);
+    }
+
+    //Получение текста со сотраницы
+    private static Set<String> parseText() throws IOException {
+        Set<String> text = new HashSet<>();
+        for (int i = 1; i <= 100; i++ ) {
+            File input = new File(String.format(PAGE_FILENAME_FORMAT, i));
+            Document doc = Jsoup.parse(input, "UTF-8");
+            text.add(doc.body().text());
+        }
+        return text;
     }
 
     private static void saveLemmas(Map<String, Set<String>> lemmasTokens) {
@@ -59,54 +83,34 @@ public class HtmlParser {
         }
     }
 
-    private static Set<String> read() {
-        try {
-            // чтение списка URL из файла в resources
-            InputStream inputStream = HtmlParser.class.getClassLoader().getResourceAsStream(HtmlParser.STOP_WORDS_FILENAME);
-            if (inputStream == null) {
-                throw new IOException("Файл " + HtmlParser.STOP_WORDS_FILENAME + " не найден в resources!");
-            }
-
-            // чтение файла построчно
-            Scanner scanner = new Scanner(inputStream);
-            Set<String> data = new HashSet<>();
-            while (scanner.hasNextLine()) {
-                data.add(scanner.nextLine());
-            }
-            scanner.close();
-            return data;
-        } catch (IOException e) {
-            throw new ResourceException();
-        }
-    }
-
-    //Лемматизация токенов
-    private static Map<String, Set<String>> lemmatizeTokens(Set<String> tokens, StanfordCoreNLP pipeline) {
+    //Лемматизация текстов
+    private static Map<String, Set<String>> lemmatizeText(Set<String> texts, StanfordCoreNLP pipeline) {
         Map<String, Set<String>> lemmatizedTokens = new TreeMap<>();
 
-        // Создаем текст из токенов
-        StringBuilder sb = new StringBuilder();
-        for (String token : tokens) {
-            sb.append(token).append(" ");
-        }
-        String text = sb.toString();
+        for (String text : texts) {
+            Annotation document = new Annotation(text);
+            pipeline.annotate(document);
 
-        // Аннотируем текст
-        Annotation document = new Annotation(text);
-        pipeline.annotate(document);
-
-        // Получаем леммы
-        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
-        for (CoreMap sentence : sentences) {
-            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
-                String word = token.word();
-                String lemma = token.get(CoreAnnotations.LemmaAnnotation.class);
-
-                lemmatizedTokens.computeIfAbsent(lemma, k -> new TreeSet<>()).add(word);
+            //Обработка отдлеьных предложений
+            List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+            for (CoreMap sentence : sentences) {
+                processSentence(sentence, lemmatizedTokens);
             }
         }
 
         return lemmatizedTokens;
+    }
+
+    private static void processSentence(CoreMap sentence, Map<String, Set<String>> lemmatizedTokens) {
+        for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+            String word = token.word().toLowerCase();
+            String lemma = token.lemma().toLowerCase();
+
+            if (!isValidWord(word) || !isValidWord(lemma)) continue;
+
+            lemmatizedTokens.computeIfAbsent(lemma, k -> new TreeSet<>()).add(word);
+        }
+        System.out.println();
     }
 
     //токенезация html файлов
@@ -138,5 +142,29 @@ public class HtmlParser {
         }
 
         return tokens;
+    }
+
+    //Фильтрация мусорных" слов (содержат не толкьо буквы, являются стоп словами, не входят в список английских слов)
+    private static boolean isValidWord(String word) {
+        return word.matches("^[a-zA-Z]+$") &&
+                !word.isEmpty() &&
+                !STOP_WORDS.contains(word) &&
+                ENGLISH_WORDS.contains(word);
+    }
+
+    //Получение ресурса
+    private static Set<String> read(String filename) {
+        try {
+            Set<String> data = new HashSet<>();
+            try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/" + filename))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    data.add(line);
+                }
+            }
+            return data;
+        } catch (IOException e) {
+            throw new ResourceException();
+        }
     }
 }
