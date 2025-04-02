5b721870d6b0377152f746d8a160ed379a3796bb
Vitaliy <komissarov.v.2003@mail.ru>
2025-04-02 14:43:25 +0300

    feat: change global tokenization to local page tokenization

--- src/main/java/ru/kpfu/itis/task2/HtmlParser.java
+++ src/main/java/ru/kpfu/itis/task2/HtmlParser.java
@@ -3,6 +3,7 @@ package ru.kpfu.itis.task2;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.Annotator;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.util.CoreMap;
 import org.jsoup.Jsoup;
@@ -22,48 +23,46 @@ public class HtmlParser {
     private static final Pattern WORD_PATTERN = Pattern.compile("\\b[a-zA-Z]+\\b");
 
     public static void main(String[] args) throws IOException {
-        HtmlParser parser = new HtmlParser();
-        parser.parse();
-    }
-
-    public void parse() throws IOException {
-        //Получения текстов со страниц
-        Set<String> texts = parseText();
-
         Properties props = new Properties();
         props.setProperty("annotators", "tokenize, ssplit, pos, lemma");
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
-        //Распределение токенов по леммам
-        Map<String, Set<String>> lemmatizedTokens = lemmatizeText(texts, pipeline);
+        HtmlParser parser = new HtmlParser();
+        parser.parseHtml(pipeline);
+    }
 
-        //Сохранение лемм
-        saveLemmas(lemmatizedTokens);
+    public void parseHtml(Annotator annotator) throws IOException {
+        for (int i = 1; i <= 100; i++) {
+            //Получение текса со страницы
+            String texts = parseText(i);
 
-        Set<String> tokens = new TreeSet<>();
+            //Распределение токенов по леммам
+            Map<String, Set<String>> lemmatizedTokens = lemmatizeText(texts, annotator);
 
-        //Сортирвока токенов
-        for (Map.Entry<String,Set<String>> entry : lemmatizedTokens.entrySet()) {
-            tokens.addAll(entry.getValue());
+            //Сохранение лемм
+            saveLemmas(lemmatizedTokens, i);
+
+            Set<String> tokens = new TreeSet<>();
+
+            //Сортирвока токенов
+            for (Map.Entry<String,Set<String>> entry : lemmatizedTokens.entrySet()) {
+                tokens.addAll(entry.getValue());
+            }
+
+            //Сохранение токенов
+            saveTokens(tokens, i);
         }
-
-        //Сохранение токенов
-        saveTokens(tokens);
     }
 
     //Получение текста со сотраницы
-    private static Set<String> parseText() throws IOException {
-        Set<String> text = new HashSet<>();
-        for (int i = 1; i <= 100; i++ ) {
-            File input = new File(String.format(PAGE_FILENAME_FORMAT, i));
-            Document doc = Jsoup.parse(input, "UTF-8");
-            text.add(doc.body().text());
-        }
-        return text;
+    private static String parseText(int pageNumber) throws IOException {
+        File input = new File(String.format(PAGE_FILENAME_FORMAT, pageNumber));
+        Document doc = Jsoup.parse(input, "UTF-8");
+        return doc.body().text();
     }
 
-    private static void saveLemmas(Map<String, Set<String>> lemmasTokens) {
-        try (FileWriter writer = new FileWriter("./result/task2/lemmas.txt")) {
+    private static void saveLemmas(Map<String, Set<String>> lemmasTokens, int pageNumber) {
+        try (FileWriter writer = new FileWriter("./result/task2/lemmas/lemmas_" + pageNumber + ".txt")) {
             for (Map.Entry<String, Set<String>> entry : lemmasTokens.entrySet()) {
                 String lemmaLine = entry.getKey() + ": " + String.join(",", entry.getValue()) + "\n";
                 writer.write(lemmaLine);
@@ -73,8 +72,8 @@ public class HtmlParser {
         }
     }
 
-    private static void saveTokens(Set<String> tokens) {
-        try (FileWriter writer = new FileWriter("./result/task2/tokens.txt")) {
+    private static void saveTokens(Set<String> tokens, int pageNumber) {
+        try (FileWriter writer = new FileWriter("./result/task2/tokens/tokens_" + pageNumber + ".txt")) {
             for (String token : tokens) {
                 writer.write(token + "\n");
             }
@@ -84,18 +83,15 @@ public class HtmlParser {
     }
 
     //Лемматизация текстов
-    private static Map<String, Set<String>> lemmatizeText(Set<String> texts, StanfordCoreNLP pipeline) {
+    private static Map<String, Set<String>> lemmatizeText(String text, Annotator annotator) {
         Map<String, Set<String>> lemmatizedTokens = new TreeMap<>();
+        Annotation document = new Annotation(text);
+        annotator.annotate(document);
 
-        for (String text : texts) {
-            Annotation document = new Annotation(text);
-            pipeline.annotate(document);
-
-            //Обработка отдлеьных предложений
-            List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
-            for (CoreMap sentence : sentences) {
-                processSentence(sentence, lemmatizedTokens);
-            }
+        //Обработка отдлеьных предложений
+        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+        for (CoreMap sentence : sentences) {
+            processSentence(sentence, lemmatizedTokens);
         }
 
         return lemmatizedTokens;
@@ -110,7 +106,6 @@ public class HtmlParser {
 
             lemmatizedTokens.computeIfAbsent(lemma, k -> new TreeSet<>()).add(word);
         }
-        System.out.println();
     }
 
     //токенезация html файлов
